642105dae0a82a9ffe3e9484ee3d66b468338c31
warathep <warathep@thinknet.co.th>
2025-10-08 15:45:59 +0700

    ✨ feat: add fake jobthai up and down func

--- release.sh
+++ release.sh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 
-TARGET_DIR="$HOME/bin"
+TARGET_DIR_BIN="$HOME/bin"
+TARGET_DIR_ZFUNC="$HOME/.zsh_functions"
 
-echo "Copying all scripts to $TARGET_DIR"
-cp -r ./*/* "$TARGET_DIR"
+echo "Copying all scripts to $TARGET_DIR_BIN"
+cp -r ./bin/*/* "$TARGET_DIR_BIN"
+echo "Done"
+
+echo "Copying all scripts to $TARGET_DIR_ZFUNC"
+cp -r ./.zsh_functions/* "$TARGET_DIR_ZFUNC"
 
 echo "Done"
