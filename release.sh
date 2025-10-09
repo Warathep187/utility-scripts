836738b4f35dc3d6fa1d8f89da445462ca246e3f
warathep <warathep@thinknet.co.th>
2025-10-09 12:00:32 +0700

    ♻️ refactor: change file position

--- release.sh
+++ release.sh
@@ -1,13 +1,13 @@
 #!/bin/zsh
 
 TARGET_DIR_BIN="$HOME/bin"
-TARGET_DIR_ZFUNC="$HOME/.zsh_functions"
+TARGET_DIR_ZSH="$HOME/.zsh"
 
-echo "Copying all scripts to $TARGET_DIR_BIN"
+echo "Copying all programs to $TARGET_DIR_BIN"
 cp -r ./bin/*/* "$TARGET_DIR_BIN"
 echo "Done"
 
-echo "Copying all scripts to $TARGET_DIR_ZFUNC"
-cp -r ./.zsh_functions/* "$TARGET_DIR_ZFUNC"
+echo "Copying utilities to $TARGET_DIR_ZSH"
+cp -r ./.zsh/* "$TARGET_DIR_ZSH"
 
 echo "Done"
